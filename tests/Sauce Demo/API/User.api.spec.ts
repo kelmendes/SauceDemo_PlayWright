96d33aa9d7e09d60d103051b435b34718a4838b9
M. Klebson <22771276+kelmendes@users.noreply.github.com>
2023-07-11 21:01:16 -0300

    organização projeto

---
+++ tests/Sauce Demo/API/User.api.spec.ts
@@ -0,0 +1,106 @@
+import { test, expect } from '@playwright/test';
+import { writeInReport } from '../Utils';
+
+test.describe('User EndPoint', () => {
+
+    test('[GET] SINGLE USER - SUCCESSFUL', async ({ request }) => {
+        // Request
+        const _reponse = await request.get(`https://reqres.in/api/users/2`);
+        // Expect Results
+        expect(_reponse.status()).toBe(200);
+        expect(_reponse.ok).toBeTruthy();
+        // Write Result in Console&Report
+        writeInReport(_reponse);
+    });
+
+    test('[GET] LIST USERS - SUCCESSFUL', async ({ request }) => {
+        // Request
+        const _reponse = await request.get(`https://reqres.in/api/users?page=2`);
+        // Expect Results
+        expect(_reponse.status()).toBe(200);
+        expect(_reponse.ok).toBeTruthy();
+        // Write Result in Console&Report
+        writeInReport(_reponse);
+    });
+
+    test('[GET] DELAYED RESPONSE - SUCCESSFUL', async ({ request }) => {
+        // Request
+        const _reponse = await request.get(`https://reqres.in/api/users?delay=4`);
+        // Expect Results
+        expect(_reponse.status()).toBe(200);
+        expect(_reponse.ok).toBeTruthy();
+        // Write Result in Console&Report
+        writeInReport(_reponse);
+
+    });
+
+    test('[GET] SINGLE USER - NOT FOUND', async ({ request }) => {
+        // Request
+        const _reponse = await request.get(`https://reqres.in/api/users/23`);
+        // Expect Results
+        expect(_reponse.status()).toBe(404);
+        // Write Result in Console&Report
+        writeInReport(_reponse);
+    });
+
+
+    test('[POST] CREATE - SUCCESSFUL', async ({ request }) => {
+        // Request
+        const _reponse = await request.post(`https://reqres.in/api/users`, {
+            data: {
+                "name": "ASDASDASD",
+                "job": "leader"
+            }
+        });
+        // Expect Results
+        expect(_reponse.status()).toBe(201);
+        // Write Result in Console&Report
+        writeInReport(_reponse);
+    });
+
+    test('[PUT] UPDATE - SUCCESSFUL', async ({ request }) => {
+        // Request
+        const _reponse = await request.put(`https://reqres.in/api/users/2`, {
+            data: {
+                "name": "morpheus",
+                "job": "zion resident"
+            }
+        });
+        // Expect Results
+        expect(_reponse.status()).toBe(200);
+        expect(_reponse.ok).toBeTruthy();
+        // Write Result in Console&Report
+        writeInReport(_reponse);
+    });
+
+    test('[PATCH] UPDATE - SUCCESSFUL', async ({ request }) => {
+        // Request
+        const _reponse = await request.patch(`https://reqres.in/api/users/2`, {
+            data: {
+                "name": "morpheus",
+                "job": "zion resident"
+            }
+        });
+        // Expect Results
+        expect(_reponse.status()).toBe(200);
+        expect(_reponse.ok).toBeTruthy();
+        // Write Result in Console&Report
+        writeInReport(_reponse);
+    });
+
+
+    test('[PATCH] DELETE - SUCCESSFUL', async ({ request }) => {
+        // Request
+        const _reponse = await request.delete(`https://reqres.in/api/users/2`, {
+            data: {
+                "name": "morpheus",
+                "job": "zion resident"
+            }
+        });
+        // Expect Results
+        expect(_reponse.status()).toBe(204);
+        // Write Result in Console&Report
+        writeInReport(_reponse);
+    });
+
+});
